8f566a8b5e0f8f24b5be2cb952483ba9b72393dc
Evan Naderi <evanvnaderi@gmail.com>
2024-02-16 10:24:03 -0500

    color changed

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,16 @@ import RightSidebar from "../components/RightSidebar";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme/theme';
 import styles from '../styles/index.module.css';
+import Head from 'next/head';
+import '../styles/global.css';
 
 const Home = () => {
     return (
         <div class={styles.main}>
+            <Head>
+                <title>LinguaFluent AI</title> 
+                {/* Any other meta tags you want can also go here */}
+            </Head>
             <ThemeProvider theme={theme} style={styles.main}>
                 <ChatContainer />
             </ThemeProvider>
